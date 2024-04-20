a5a39dd38262ebe1f0a6e01ecde5fffd01c75865
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-04-20 16:15:44 -0500

    feat: :art: three new dependencies
    three new dependencies: wouter, sonner & boxicons

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,25 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:react-hooks/recommended",
-    "prettier",
-  ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  settings: { react: { version: "18.2" } },
-  plugins: ["react-refresh", "prettier"],
-  rules: {
-    "react/jsx-no-target-blank": "off",
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
-    "no-console": 1,
-    "prettier/prettier": 2,
-  },
-};
+	root: true,
+	env: { browser: true, es2020: true },
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'plugin:react-hooks/recommended',
+		'prettier',
+	],
+	ignorePatterns: ['dist', '.eslintrc.cjs'],
+	parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+	settings: { react: { version: '18.2' } },
+	plugins: ['react-refresh', 'prettier'],
+	rules: {
+		'react/jsx-no-target-blank': 'off',
+		'react-refresh/only-export-components': [
+			'warn',
+			{ allowConstantExport: true },
+		],
+		'react/prop-types': 'off',
+		'no-console': 1,
+		'prettier/prettier': 2,
+	},
+}
